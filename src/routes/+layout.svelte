99009d53a6aca562a605a74197046fa881845830
Benedict Halim <benedicthalim1@gmail.com>
2025-04-13 08:04:46 +0700

    feat github static site

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,36 @@
+<script lang="js">
+    import { base } from "$app/paths";
+</script>
+
+<nav>
+    <a href="{base}/">Home</a>
+    <!-- <a href="{base}/about">About</a> -->
+</nav>
+
+<main>
+    <slot />
+</main>
+
+<style>
+    nav {
+        max-width: 40rem;
+        margin: auto;
+        padding: 1rem;
+        display: flex;
+        gap: 1rem;
+    }
+
+    main {
+        max-width: 40rem;
+        margin: auto;
+        padding: 0 1rem;
+    }
+
+    main > *:not(:last-child) {
+        margin-bottom: 1rem;
+    }
+
+    :global(img) {
+        max-width: 100%;
+    }
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -3,20 +3,14 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: vitePreprocess(),
+
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false,
-			strict: true,
-		}),
-		appDir: '_app',
+		adapter: adapter(),
 		paths: {
-			base: '/Hueku' // Use paths.base instead of base directly
+			base: process.env.NODE_ENV === 'production' ? '/Hueku' : '',
 		}
-	},
-	preprocess: vitePreprocess()
+	}
 };
 
 export default config;
